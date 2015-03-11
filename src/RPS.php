9eba52366b31619541e1c408f5915fd3236ddce3
Connor Abdelnoor & Bryan Borgeson <student@epicodus.com>
2015-03-11 10:59:49 -0700

    Add comments to clarify what's happening in the source file.

--- src/RPS.php
+++ src/RPS.php
@@ -3,48 +3,13 @@
     class RPS
     {
 
-        // function rpsResult($input1, $input2)
-        // {
-        //     $input1 = ucfirst(strtolower($input1));
-        //     $input2 = ucfirst(strtolower($input2));
-        //
-        //
-        //     if($input1 == $input2){
-        //
-        //         return "DRAW!";
-        //
-        //     } elseif ($input1 == "Rock" && $input2 == "Paper") {
-        //
-        //         return "Player 2 Wins!";
-        //
-        //     } elseif ($input1 == "Paper" && $input2 == "Rock") {
-        //
-        //         return "Player 1 Wins!";
-        //
-        //     } elseif ($input1 == "Rock" && $input2 == "Scissors"){
-        //
-        //         return "Player 1 Wins!";
-        //
-        //     } elseif ($input1 == "Scissors" && $input2 == "Rock"){
-        //
-        //         return "Player 2 Wins!";
-        //
-        //     } elseif ($input1 == "Paper" && $input2 == "Scissors") {
-        //
-        //         return "Player 2 Wins!";
-        //
-        //     } elseif ($input1 == "Scissors" && $input2 == "Paper"){
-        //
-        //         return "Player 1 Wins!";
-        //     }
-        // }
-
         function rpsResult($input1, $input2)
         {
-            
+            // Control users input capitalization
             $input1 = ucfirst(strtolower($input1));
             $input2 = ucfirst(strtolower($input2));
 
+            // Declare Winners and Draw (when both users input the same thing)
             if($input1 == $input2) {
 
                 return "DRAW!";
