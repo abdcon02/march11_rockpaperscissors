84e0037f8a3e961984e1a6f902cf5b7b485f6e21
Connor Abdelnoor & Bryan Borgeson <student@epicodus.com>
2015-03-11 14:13:59 -0700

    total refraction of RPS (I hope)

--- app/app.php
+++ app/app.php
@@ -20,12 +20,15 @@
 
         if (!isset($_POST['player2'])) {
             $player2 = $_POST['player2'] = $winner->rpsSinglePlayer();
+            $checker = 1;
+        } else {
+            $player2 = $_POST['player2'];
+            $checker = "";
         }
 
         $win = $winner->rpsResult($_POST['player1'], $player2);
 
-        return $app['twig']->render('winner.twig',
-        array('winner' => $win, 'player1' => $_POST['player1'], 'player2' => $player2));
+        return $app['twig']->render('winner.twig', array('winner' => $win, 'player1' => $_POST['player1'], 'player2' => $player2, 'check' => $checker));
 
     });
 
